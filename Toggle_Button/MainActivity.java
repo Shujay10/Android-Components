c7b7220f39acd86405656eb3b6c5de4debe7b4b3
me_jaiiii <57563664+me-jaii@users.noreply.github.com>
2023-06-26 23:55:55 +0530

    Toggle_Button

---
+++ Toggle_Button/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.togglebutton;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleButton1, toggleButton2, toggleButton3;
+    private TextView textView1, textView2, textView3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton1 = findViewById(R.id.toggleButton1);
+        toggleButton2 = findViewById(R.id.toggleButton2);
+        toggleButton3 = findViewById(R.id.toggleButton3);
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+
+        toggleButton1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String message = isChecked ? "Toggle Button 1 is ON" : "Toggle Button 1 is OFF";
+                showToast(message);
+            }
+        });
+
+        toggleButton2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String message = isChecked ? "Toggle Button 2 is ON" : "Toggle Button 2 is OFF";
+                showToast(message);
+            }
+        });
+
+        toggleButton3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String message = isChecked ? "Toggle Button 3 is ON" : "Toggle Button 3 is OFF";
+                showToast(message);
+            }
+        });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
